3341f60af5b62b82e5a3699083925510df08d597
jochanik <whcksdlr5909@naver.com>
2023-10-26 01:41:12 +0900

    <refactor>[#136-JCI] merge 과정 충돌난 부분 코드 수정

--- client/src/pages/GameListPage.tsx
+++ client/src/pages/GameListPage.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import NavBar from '../components/NavBar/NavBar';
+import NavBar from 'components/common/NavBar/NavBar';
 import Game from '../components/Game/Game';
 
 function GameListPage() {
