528d4f6bf9a4c9c065dffcb0bc8dc7a73931b13e
김민국 <eoals6511@naver.com>
2023-10-25 17:58:22 +0900

    <first>[#143-KMK] 회원가입 페이지 초본 생성

--- client/src/pages/LoginPage.tsx
+++ client/src/pages/LoginPage.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-/* eslint-disable import/prefer-default-export */
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import Login from '../components/Login/Login';
 import SignUp from '../components/SignUp/SignUp';
@@ -14,14 +13,38 @@ const LoginBg = styled.div`
 		background-position: center;
 		background-repeat: no-repeat;
 		display: flex;
+		justify-content: center;
+	}
+	.page-box {
+		background-color: white;
+		height: 70%;
+		opacity: 0.9;
+		border-radius: 30px;
+	}
+	.change-bar {
+		display: flex;
+		justify-content: space-between;
 	}
 `;
+
 function LoginPage() {
+	const [condition, setCondition] = useState('login');
+
 	return (
 		<LoginBg>
 			<div className="bgimage">
-				<Login />
-				<SignUp />
+				<div className="page-box">
+					<div className="change-bar">
+						<button type="submit" onClick={() => setCondition('login')}>
+							로그인
+						</button>
+						<button type="submit" onClick={() => setCondition('signup')}>
+							회원가입
+						</button>
+					</div>
+					{condition === 'login' && <Login />}
+					{condition === 'signup' && <SignUp />}
+				</div>
 			</div>
 		</LoginBg>
 	);
