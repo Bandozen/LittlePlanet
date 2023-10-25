0b946001f5b1a685c09258e9481919cf6ffb4513
wisdomx2 <wisdomx2@hanmail.net>
2023-10-25 18:04:53 +0900

    <feat>[#153-YJH] 콘텐츠 소개페이지 생성

--- client/src/components/Button/style.ts
+++ client/src/components/Button/style.ts
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 
 export const Button = styled.div`
 	.play-btn {
-		font-size: 1.5vw;
+		font-size: 1.2rem;
 		background-color: #faff00;
 		color: black;
 		border: none;
@@ -21,5 +21,29 @@ export const Button = styled.div`
 			background-color: #188eb7;
 			color: white;
 		}
+	}
+	.detail-btn {
+		font-size: 1.2rem;
+		background-color: white;
+		color: black;
+		border: none;
+		padding: 12px 24px;
+		border-radius: 40px;
+		cursor: pointer;
+		transition: background-color 0.3s;
+		width: 6vw;
+		height: 4.5vh;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+
+		&:hover {
+			color: #188eb7;
+		}
+		// 테스트해보기
+		// &.active {
+		// 	color: #188eb7;
+		// 	font-weight: bold;
+		// }
 	}
 `;
