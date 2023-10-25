33b66b8cbbd7a1d39156962c28b60523ab9728bc
wisdomx2 <wisdomx2@hanmail.net>
2023-10-25 12:18:42 +0900

    <feat>[#153-YJH] 시뮬레이션, 게임페이지 레이아웃 및 더미생성

--- client/src/components/Game/Game.tsx
+++ client/src/components/Game/Game.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import PlayButton from 'components/Button/PlayButton';
 import { GameWrapper } from './style';
+import games from '../../dummys/games';
 
 function Game() {
 	return (
 		<GameWrapper>
-			<div>게임 페이지</div>
-			<PlayButton />
+			<div>
+				{games.map((game) => (
+					<div key={game.id} className="game-item">
+						<img src={game.imageUrl} alt={game.name} />
+						<div>
+							<h2>{game.name}</h2>
+							<p>{game.description}</p>
+						</div>
+						<PlayButton />
+					</div>
+				))}
+			</div>
 		</GameWrapper>
 	);
 }
