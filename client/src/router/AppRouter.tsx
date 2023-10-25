b9b6c6ce505c17a91ae9db6ee235f246fa25f849
jochanik <whcksdlr5909@naver.com>
2023-10-26 01:22:38 +0900

    <style>[#136-JCI]파일 위치 수정

--- client/src/router/AppRouter.tsx
+++ client/src/router/AppRouter.tsx
@@ -4,6 +4,7 @@ import { GlobalFonts } from 'styles/GlobalFonts';
 import { GlobalStyles } from 'styles/GlobalStyles';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import MainPage from 'pages/MainPage';
+import SimulationMachinePage from 'pages/SimulationMachinePage';
 
 function AppRouter() {
 	return (
@@ -13,6 +14,7 @@ function AppRouter() {
 			<BrowserRouter>
 				<Routes>
 					<Route path="/" element={<MainPage />} />
+					<Route path="/simulationmachine" element={<SimulationMachinePage />} />
 				</Routes>
 			</BrowserRouter>
 		</RecoilRoot>
