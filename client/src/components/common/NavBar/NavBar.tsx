41d9466d594f2de744120a37420d169aea4d3d83
jochanik <whcksdlr5909@naver.com>
2023-10-26 01:41:12 +0900

    <refactor>[#136-JCI] merge 과정 충돌난 부분 코드 수정

--- client/src/components/common/NavBar/NavBar.tsx
+++ client/src/components/common/NavBar/NavBar.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import logo from 'assets/images/logo.png';
 import { NavBarWrapper, NavBarLink } from './style';
-import logo from '../../assets/logo.png';
 
 function NavBar() {
 	return (
