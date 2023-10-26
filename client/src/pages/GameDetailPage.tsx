cddf232dda9e9135e55714114e525191754081c0
wisdomx2 <wisdomx2@hanmail.net>
2023-10-26 09:06:26 +0900

    <feat>[#153-YJH] 콘텐츠디테일페이지 생성

---
+++ client/src/pages/GameDetailPage.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import NavBar from '../components/NavBar/NavBar';
+// import Game from '../components/Game/Game';
+
+function GameDetailPage() {
+	return (
+		<div>
+			<NavBar />
+			<hr />
+			<h1>여긴 게임디테일페이지!</h1>
+			{/* <Game /> */}
+		</div>
+	);
+}
+
+export default GameDetailPage;
---
+++ client/src/pages/SimulationDetailPage.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import NavBar from '../components/NavBar/NavBar';
+// import Simulation from '../components/Simulation/Simulation';
+
+function SimulationDetailPage() {
+	return (
+		<div>
+			<NavBar />
+			<hr />
+			<h1>여긴 시뮬레이션 디테일페이지!</h1>
+			{/* <Simulation /> */}
+		</div>
+	);
+}
+
+export default SimulationDetailPage;
