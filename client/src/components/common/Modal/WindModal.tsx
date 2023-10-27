dba28666f2afcf4b163323ae32d6c66c623bdf3e
wisdomx2 <wisdomx2@hanmail.net>
2023-10-27 14:16:51 +0900

    <feat>[#153-YJH] 모달생성

--- client/src/components/common/Modal/WindModal.tsx
+++ client/src/components/common/Modal/WindModal.tsx
@@ -1,54 +1,28 @@
 import React from 'react';
-import { Button, Dialog, Card, CardBody, CardFooter, Typography, Input, Checkbox } from '@material-tailwind/react';
+import { Dialog, Card, CardHeader, CardBody, Typography } from '@material-tailwind/react';
 
-export function DialogWithForm() {
-	const [open, setOpen] = React.useState(false);
-	const handleOpen = () => setOpen((cur) => !cur);
+interface IModalProps {
+	title: string;
+	content: JSX.Element | string;
+	open: boolean;
+	handleClose: () => void;
+}
 
+function WindModal({ title, content, open, handleClose }: IModalProps) {
 	return (
-		<>
-			<Button onClick={handleOpen}>Sign In</Button>
-			<Dialog size="xs" open={open} handler={handleOpen} className="bg-transparent shadow-none">
-				<Card className="mx-auto w-full max-w-[24rem]">
-					<CardBody className="flex flex-col gap-4">
-						<Typography variant="h4" color="blue-gray">
-							Sign In
-						</Typography>
-						<Typography className="mb-3 font-normal" variant="paragraph" color="gray">
-							Enter your email and password to Sign In.
-						</Typography>
-						<Typography className="-mb-2" variant="h6">
-							Your Email
-						</Typography>
-						<Input label="Email" size="lg" crossOrigin="" />
-						<Typography className="-mb-2" variant="h6">
-							Your Password
-						</Typography>
-						<Input label="Password" size="lg" crossOrigin="" />
-						<div className="-ml-2.5 -mt-3">
-							<Checkbox label="Remember Me" crossOrigin="" />
-						</div>
-					</CardBody>
-					<CardFooter className="pt-0">
-						<Button variant="gradient" onClick={handleOpen} fullWidth>
-							Sign In
-						</Button>
-						<Typography variant="small" className="mt-4 flex justify-center">
-							Don&apos;t have an account?
-							<Typography
-								as="a"
-								href="#signup"
-								variant="small"
-								color="blue-gray"
-								className="ml-1 font-bold"
-								onClick={handleOpen}
-							>
-								Sign up
-							</Typography>
-						</Typography>
-					</CardFooter>
-				</Card>
-			</Dialog>
-		</>
+		<Dialog size="xs" open={open} handler={handleClose} className="bg-transparent shadow-none">
+			<Card className="mx-auto w-full max-w-[24rem]">
+				<CardHeader color="yellow" className="p-4 w-full">
+					<Typography variant="h5" className="text-black">
+						{title}
+					</Typography>
+				</CardHeader>
+				<CardBody>
+					<Typography>{content}</Typography>
+				</CardBody>
+			</Card>
+		</Dialog>
 	);
 }
+
+export default WindModal;
---
+++ client/src/components/simualtionapplication/DownloadModal/DownloadModal.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import useOpenModal from 'hooks/useModalOpen';
+import Button from 'components/common/Button';
+import WindModal from 'components/common/Modal/WindModal';
+import tempQR from 'assets/images/temp_qr.png';
+import { DownloadModalWrapper } from './style';
+
+const modalContent = (
+	<div className="flex flex-column justify-center">
+		<p>QR코드를 스캔하여 어플을 다운받아보세요!</p>
+		<img src={tempQR} alt="큐알이에요" style={{ width: '70%', marginBottom: '20px' }} />
+	</div>
+);
+function OpenModal() {
+	const { isOpenModal, clickModal, closeModal } = useOpenModal();
+
+	return (
+		<>
+			<DownloadModalWrapper>
+				<Button text="앱 다운로드" handleClick={() => clickModal()} />
+			</DownloadModalWrapper>
+			<WindModal open={isOpenModal} handleClose={closeModal} title="소행성 어플" content={modalContent} />
+		</>
+	);
+}
+
+export default OpenModal;
