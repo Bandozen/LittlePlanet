5206a603ae415c46e614d44ff0c8b800ec9bee62
wisdomx2 <wisdomx2@hanmail.net>
2023-10-30 17:55:04 +0900

    <feat>[#153-YJH]  STT 컴포넌트 생성

--- client-app/src/Components/PhoneKey/PhoneKey.tsx
+++ client-app/src/Components/PhoneKey/PhoneKey.tsx
@@ -1,7 +1,16 @@
 import React, { useState, useEffect } from 'react';
+import { StackNavigationProp } from '@react-navigation/stack';
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import { MaterialIcons } from '@expo/vector-icons';
 import { Audio } from 'expo-av';
+
+type RootStackParamList = {
+  Main: undefined;
+  PhoneKey: undefined;
+  Calling: { phoneNumber: string };
+};
+
+type PhoneKeyNavigationProp = StackNavigationProp<RootStackParamList, 'PhoneKey'>;
 
 type KeypadButtonProps = {
     number: string | number;
@@ -10,6 +19,7 @@ type KeypadButtonProps = {
 
 interface PhoneKeyProps {
     onEndCall: () => void;
+    navigation: PhoneKeyNavigationProp;
   }
 
 const KeypadButton: React.FC<KeypadButtonProps> = ({ number, onPress }) => {
@@ -21,17 +31,32 @@ const KeypadButton: React.FC<KeypadButtonProps> = ({ number, onPress }) => {
   );
 };
 
-const PhoneKey: React.FC<PhoneKeyProps> = ({ onEndCall }) => {
+const PhoneKey: React.FC<PhoneKeyProps> = ({ onEndCall, navigation }) => {
     const [number, setNumber] = useState<string>('');
     const [sound, setSound] = useState<Audio.Sound | null>(null);
-  
+
+    // 키패드 효과음
+    const playKeyPressSound = async () => {
+      const { sound } = await Audio.Sound.createAsync(
+        require('../../../assets/music/keysound2.mp3')
+      );
+      await sound.playAsync();
+      sound.setOnPlaybackStatusUpdate((status) => {
+        if ('isLoaded' in status && status.isLoaded && status.didJustFinish) {
+          sound.unloadAsync();
+        }
+      });
+    };
+    
     const handleKeyPress = (key: string | number) => {
+      playKeyPressSound();
       setNumber((prev) => prev + key);
     };
   
+    // 통화연결음
     const playSound = async () => {
       const { sound } = await Audio.Sound.createAsync(
-         require('../../../assets/music/callingmusic.mp3')
+         require('../../../assets/music/ringingsound.mp3')
       );
       setSound(sound);
       await sound.playAsync(); 
@@ -46,8 +71,9 @@ const PhoneKey: React.FC<PhoneKeyProps> = ({ onEndCall }) => {
     };
   
     const handleCall = () => {
-      alert(number + '로 전화를 거는 중입니다...');
+      // alert(number + '로 전화를 거는 중입니다...');
       playSound();
+      navigation.navigate("Calling", { phoneNumber: number});
     };
   
      useEffect(() => {
