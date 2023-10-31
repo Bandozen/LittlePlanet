1cb80d4b612b3e7e814a274afb556a634734510a
위효선 <wizardisme@naver.com>
2023-10-31 17:31:48 +0900

    <feat>[#216-WHS] 라우터, 히스토리 조회 및 생성 수정

--- server/src/main/java/project/c203/server/simulationHistory/repository/SimulationHistoryRepository.java
+++ server/src/main/java/project/c203/server/simulationHistory/repository/SimulationHistoryRepository.java
@@ -4,7 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import project.c203.server.simulationHistory.entity.SimulationHistory;
 
+import java.util.List;
+
 @Repository
 public interface SimulationHistoryRepository extends JpaRepository<SimulationHistory, Integer> {
-    boolean existsSimulationHistoryBySimulationSeqAndAndStudent_StudentSeq(Integer simulationSeq, Integer studentSeq);
+    List<SimulationHistory> findSimulationHistoriesBySimulationSeqAndMember_MemberEmail(Integer simulationSeq, String memberEmail);
+
+    boolean existsSimulationHistoryBySimulationSeqAndStudent_StudentSeq(Integer simulationSeq, Integer studentSeq);
 }
