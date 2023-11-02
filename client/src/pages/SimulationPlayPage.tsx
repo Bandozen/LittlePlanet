eae130085bc73bc0becdd7c604322dd040e542af
jochanik <whcksdlr5909@naver.com>
2023-11-02 09:15:36 +0900

    <feat>[#199-JCI]시뮬레이션 플레이 화면 1차 구현중

---
+++ client/src/pages/SimulationPlayPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SimulationChat from 'components/simsulationplay/SimulationChat';
+
+function SimulationPlayPage() {
+	return (
+		<div>
+			<SimulationChat />
+		</div>
+	);
+}
+
+export default SimulationPlayPage;
