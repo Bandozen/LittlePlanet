b1de7190d3fd83a031455b1bd3e3b12f1e940dc1
wisdomx2 <wisdomx2@hanmail.net>
2023-11-02 22:42:10 +0900

    <feat>[#153-YJH] 앱 로그인,로그아웃 생성

--- LittlePlanet/android/app/src/utils/MemberAPI.tsx
+++ LittlePlanet/android/app/src/utils/MemberAPI.tsx
@@ -1,18 +1,56 @@
-import axios from 'axios';
-
-const BASE_URL = 'https://k9c203.p.ssafy.io/api/v1/member/app/login';
+const BASE_URL = 'http://10.0.2.2:8080/api/v1/member';
 
 export const MemberAPI = {
   login: async (email: string, password: string) => {
+    console.log('이메일', email);
+    console.log('비번', password);
     try {
-      const response = await axios.post(`${BASE_URL}/login`, {
-        email,
-        password,
+      const response = await fetch(`${BASE_URL}/login`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          memberEmail: email,
+          memberPassword: password,
+        }),
       });
-      return response.data;
+
+      // 응답 헤더에서 쿠키 추출
+      const cookies = response.headers.get('Set-Cookie');
+      console.log('Received cookies:', cookies);
+
+      // 쿠키 문자열에서 JWT를 추출할 때 "Object is possibly 'undefined'" 오류를 해결하기 위한 검사
+      let jwt = '';
+      if (cookies) {
+        const jwtMatch = cookies.match(/JWT=([^;]+)/);
+        jwt = jwtMatch ? jwtMatch[1] : '';
+        console.log('JWT:', jwt);
+      }
+
+      // 응답 본문을 JSON으로 변환
+      const data = await response.json();
+
+      // 추출된 JWT와 데이터를 반환
+      return {jwt, data};
     } catch (error) {
+      console.error('Error:', error);
+      throw error;
+    }
+  },
+
+  logout: async () => {
+    try {
+      const response = await fetch(`${BASE_URL}/logout`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      return response.json();
+    } catch (error) {
+      console.error('Error:', error);
       throw error;
     }
   },
 };
-
