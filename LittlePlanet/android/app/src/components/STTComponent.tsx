2ff8ec72e096a3b46b58ce59f1c7db5d2a5e2823
wisdomx2 <wisdomx2@hanmail.net>
2023-11-04 11:53:54 +0900

    <feat>[#153-YJH] 나레이션 재생 및 STT 웹소켓 테스트로직  생성

--- LittlePlanet/android/app/src/components/STTComponent.tsx
+++ LittlePlanet/android/app/src/components/STTComponent.tsx
@@ -1,21 +1,15 @@
-import React, {useState, useEffect} from 'react';
-import {View, Text, Button} from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, Button } from 'react-native';
 import Voice from '@react-native-community/voice';
 
-const STTComponent: React.FC = () => {
+interface STTComponentProps {
+  onSTTResult: (text: string) => void;
+}
+
+// 부모 컴포넌트(CallingComponent)로부터 onSTTResult 콜백을 받아 음성 인식 결과를 전달
+const STTComponent: React.FC<STTComponentProps> = ({ onSTTResult }) => {
   const [isRecording, setIsRecording] = useState<boolean>(false);
   const [text, setText] = useState<string>('');
-
-  useEffect(() => {
-    Voice.onSpeechStart = onSpeechStart;
-    Voice.onSpeechEnd = onSpeechEnd;
-    Voice.onSpeechResults = onSpeechResults;
-    Voice.onSpeechPartialResults = onSpeechPartialResults;
-
-    return () => {
-      Voice.destroy().then(Voice.removeAllListeners);
-    };
-  }, []);
 
   const onSpeechStart = () => {
     setIsRecording(true);
@@ -27,12 +21,27 @@ const STTComponent: React.FC = () => {
   };
 
   const onSpeechResults = (event: any) => {
-    setText(event.value[0]);
+    const textResult = event.value[0];
+    setText(textResult);
+    onSTTResult(textResult);
   };
 
   const onSpeechPartialResults = (event: any) => {
     setText(event.value[0]);
-};
+  };
+
+  useEffect(() => {
+    Voice.onSpeechStart = onSpeechStart;
+    Voice.onSpeechEnd = onSpeechEnd;
+    Voice.onSpeechResults = onSpeechResults;
+    Voice.onSpeechPartialResults = onSpeechPartialResults;
+
+    return () => {
+      Voice.destroy().then(Voice.removeAllListeners);
+    };
+  // onSTTResult를 의존성 배열에 추가
+  // CallingComponent에서 onSTTResult가 변경되면 STTComponent에도 반영되어 리스너 업데이트됨
+  }, [onSTTResult]);
 
   const startRecording = async () => {
     try {
@@ -51,14 +60,16 @@ const STTComponent: React.FC = () => {
   };
 
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'black' }}>
       <Button
         title={isRecording ? '녹음 중지' : '녹음 시작'}
         onPress={isRecording ? stopRecording : startRecording}
       />
-      <Text>{text}</Text>
+      <Text style={{ color: 'white', padding: 10 }}>{text}</Text>
     </View>
   );
 };
 
 export default STTComponent;
+
+
