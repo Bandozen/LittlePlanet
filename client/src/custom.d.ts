6632a606f82ef359f2a98828c1a2e3716bbbdeae
jochanik <whcksdlr5909@naver.com>
2023-11-06 16:01:46 +0900

    <fix>[#001-JCI]모듈 mp3형식 파일 추가

--- client/src/custom.d.ts
+++ client/src/custom.d.ts
@@ -3,6 +3,7 @@ declare module '*.svg';
 declare module '*.png';
 declare module '*.jpeg';
 declare module '*.gif';
+declare module '*.mp3';
 
 // heroicon 관련
 declare module '@heroicons/react/outline';
