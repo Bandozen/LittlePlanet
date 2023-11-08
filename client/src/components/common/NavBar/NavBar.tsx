e1eb7d991e6925469e6bda776b84b9f53fd78540
서정빈 <jeongb9716@gmail.com>
2023-11-08 14:36:50 +0900

    <fix>[#258-SJB] 소켓통신 최적화

--- client/src/components/common/NavBar/NavBar.tsx
+++ client/src/components/common/NavBar/NavBar.tsx
@@ -1,14 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useRecoilValue, useSetRecoilState } from 'recoil';
 import logo from 'assets/images/logo.png';
 import api from '../../../api';
 import { NavBarWrapper, NavBarLink } from './style';
+import { userEmail } from '../../../store/RecoilState';
 
 function NavBar() {
+	const userMail = useRecoilValue(userEmail);
+	const setUserMail = useSetRecoilState(userEmail);
+
 	const handleLogout = async () => {
 		try {
 			const response = await api.post('/member/logout');
+			await api.post('/member/command', {
+				memberEmail: userMail,
+				memberCommand: 'logout',
+			});
 			console.log(response);
+			setUserMail('');
 		} catch (error) {
 			console.log(error);
 		}
