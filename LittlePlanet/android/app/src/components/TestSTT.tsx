17361fd237204a1b65030978e072d09d0a90234e
wisdomx2 <wisdomx2@hanmail.net>
2023-11-09 09:34:30 +0900

    <feat>[#153-YJH] STT 최종텍스트 send 테스트

--- LittlePlanet/android/app/src/components/TestSTT.tsx
+++ LittlePlanet/android/app/src/components/TestSTT.tsx
@@ -16,55 +16,46 @@ const TestSTT: React.FC<TestSTTProps> = ({
 }) => {
   const [isRecording, setIsRecording] = useState<boolean>(false);
   const [text, setText] = useState<string>('');
-  const [timeoutId, setTimeoutId] = useState<NodeJS.Timeout | null>(null);
   const [isFinal, setIsFinal] = useState<string>('');
-  // 타임딜레이 주는 함수
-  const stopRecordingWithDelay = async (delay: number) => {
-    // 이미 설정된 타임아웃을 취소
-    if (timeoutId) clearTimeout(timeoutId);
 
-    // 새로운 타임아웃을 설정
-    const newTimeoutId = setTimeout(async () => {
-      try {
-        await Voice.stop();
-        setIsRecording(false); // 음성 인식이 멈춘 후에 isRecording 상태를 업데이트
-      } catch (e) {
-        console.error(e);
-      }
-    }, delay);
-
-    // 새로운 타임아웃 ID를 저장
-    setTimeoutId(newTimeoutId);
-  };
 
   const onSpeechStart = () => {
     setIsRecording(true);
     setText('');
   };
-
+  
   const onSpeechEnd = () => {
-    onSTTResult(text);
-    console.log('최최종말끝남', 'text');
-    // 즉시 음성 인식을 멈추지 않고, 지연 시간 후에 멈춤
-    stopRecordingWithDelay(3000); // 3초 후에 음성 인식을 멈추도록 설정
+    if (text) {
+      console.log('최최종말끝남텍스트먼데', text);
+      onSTTResult(text); // text가 비어있지 않다면 부모 컴포넌트에 전달
+    } else {
+      console.log('음성 인식 결과가 아직 준비되지 않았습니다.');
+    }
     setIsRecording(false);
   };
   // 음성 인식의 임시 결과를 처리하는 함수
   const onSpeechPartialResults = (event: any) => {
     // event.value는 배열로 임시 결과를 포함하고 있음.
-    console.log('임시말들:', event.value);
+    console.log('onSpeechPartialResults의 event.value:', event.value);
     // setText(event.value[0]);
   };
-
-  // 최종 결과를 처리하는 함수
   const onSpeechResults = (event: any) => {
-    console.log('최종말들', event.value);
-    // 최종 결과를 setText로 설정하고 onSTTResult 콜백을 호출
+    console.log('onSpeechResults의 event.value', event.value);
+    // 최종 결과만을 setText로 설정
     let finalResult = event.value.join(' ');
-    setText(prevText => prevText + ' ' + event.value.join(' '));
-    console.log('최종말들 이어졌나요?', finalResult);
-    onSTTResult(finalResult);
+    setText(finalResult); // 이전 텍스트에 더하지 않고 새로운 값을 설정
+    console.log('조인된 finalResult?', finalResult);
+    onSTTResult(finalResult); // 부모 컴포넌트에 결과 전달
   };
+  // // 최종 결과를 처리하는 함수
+  // const onSpeechResults = (event: any) => {
+  //   console.log('최종말들', event.value);
+  //   // 최종 결과를 setText로 설정하고 onSTTResult 콜백을 호출
+  //   let finalResult = event.value.join(' ');
+  //   setText(prevText => prevText + ' ' + event.value.join(' '));
+  //   console.log('최종말들 이어졌나요?', finalResult);
+  //   onSTTResult(finalResult);
+  // };
   // const onSpeechResults = (event: any) => {
   //   const textResult = event.value[0];
   //   setText(textResult);
@@ -75,8 +66,11 @@ const TestSTT: React.FC<TestSTTProps> = ({
   //   setText(event.value[0]);
   // };
   useEffect(() => {
-    setIsFinal(text);
-  }, [text]);
+    // text 상태가 변경될 때만 socketSend를 호출
+    if (text) {
+      socketSend(text);
+    }
+  }, [text]); // text 상태가 변경될 때마다 실행
 
   useEffect(() => {
     // 리스너 추가
@@ -93,13 +87,11 @@ const TestSTT: React.FC<TestSTTProps> = ({
         stopRecording().catch(console.error);
       }
     }
-    // 컴포넌트가 언마운트될 때 리스너 제거 및 타이머 취소
+    // 컴포넌트가 언마운트될 때 리스너 제거 
     return () => {
-      // clearTimeout(timerId); // 10초 타이머 취소
-      if (timeoutId) clearTimeout(timeoutId); // 타임아웃 취소
       Voice.destroy().then(Voice.removeAllListeners);
     };
-  }, [isSTTActive, timeoutId]);
+  }, [isSTTActive]);
 
   useEffect(() => {
     socketSend(text);
@@ -108,9 +100,6 @@ const TestSTT: React.FC<TestSTTProps> = ({
   const startRecording = async () => {
     try {
       await Voice.start('ko-KR');
-      // if (isRecording) {
-      //   stopRecording();
-      // }
     } catch (e) {
       console.error(e);
     }
