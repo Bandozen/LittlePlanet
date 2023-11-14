d097ed9eec3d941b2af2bb5630a259045336ebab
위효선 <wizardisme@naver.com>
2023-11-14 11:38:29 +0900

    <feat>[#349-WHS] 시뮬레이션 1번, 4번 소방관 나레이션 오디오 넣기 인트로 생성, 관련 기능 추가

--- client/src/custom.d.ts
+++ client/src/custom.d.ts
@@ -4,6 +4,7 @@ declare module '*.png';
 declare module '*.jpeg';
 declare module '*.gif';
 declare module '*.mp3';
+declare module '*.mp4';
 
 // heroicon 관련
 declare module '@heroicons/react/outline';
