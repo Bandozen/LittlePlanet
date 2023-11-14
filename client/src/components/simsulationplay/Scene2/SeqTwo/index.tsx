c77afed736b886cb88c8df7922b10daa455fbd99
jochanik <whcksdlr5909@naver.com>
2023-11-14 11:46:35 +0900

    <feat>]#356-JCI]특정 장소 도달 시 alert창 띄우기 구현

--- client/src/components/simsulationplay/Scene2/SeqTwo/index.tsx
+++ client/src/components/simsulationplay/Scene2/SeqTwo/index.tsx
@@ -1,6 +1,6 @@
 import React, { Dispatch, SetStateAction, useState } from 'react';
-import { Button } from '@material-tailwind/react';
-import Button1 from 'components/common/Button';
+import { Alert, Button, Typography } from '@material-tailwind/react';
+import { InformationCircleIcon } from '@heroicons/react/24/outline';
 import CharacterDisplay from 'components/CharacterDisplay';
 import ShineLottie from 'components/Lottie/ShineLottie';
 import PointRightLottie from 'components/Lottie/PointRightLottie';
@@ -17,18 +17,34 @@ function SeqTwo(props: ISeqTwoProps) {
 	// 캐릭터 초기 위치세팅
 	const [left, setLeft] = useState(500);
 
+	// 장소 위치 세팅
+	const [littleplanetInfo, setLittleplanetInfo] = useState(false);
+	const [buildingInfo, setBuildingInfo] = useState(false);
+
 	const handleLeft = () => setLeft((prevLeft) => prevLeft - 5);
 	const handleRight = () => setLeft((prevLeft) => prevLeft + 5);
 
-	const testClick = () => {
-		setStep(2);
+	const littlePlanet = () => {
+		setLittleplanetInfo((prev) => !prev);
+		const timer = setTimeout(() => {
+			setStep(2);
+		}, 5000);
+
+		return () => {
+			clearTimeout(timer);
+		};
+	};
+
+	const samsungBuilding = () => {
+		setBuildingInfo((prev) => !prev);
 	};
 
 	return (
 		<SeqTwoWrapper>
 			<Button onClick={handleLeft}>왼쪽</Button>
 			<Button onClick={handleRight}>오른쪽</Button>
-			<Button1 text="테스트버튼입니다." handleClick={() => testClick()} />
+			<Button onClick={littlePlanet}>소행성로</Button>
+			<Button onClick={samsungBuilding}>삼성빌딩</Button>
 			<div style={{ position: 'absolute', left: `${left}px` }}>
 				<CharacterDisplay />
 			</div>
@@ -41,6 +57,32 @@ function SeqTwo(props: ISeqTwoProps) {
 			<div style={{ position: 'absolute' }} className="right-point">
 				<PointRightLottie />
 			</div>
+			{littleplanetInfo && (
+				<div className="alert-container">
+					<Alert className="flex justify-center">
+						<div className="flex flex-row items-center m-2">
+							<InformationCircleIcon className="w-8 h-8 mr-2" color="yellow" />
+							<Typography variant="h3">여기는 소행성로 203번이에요.</Typography>
+						</div>
+						<div className="flex justify-center">
+							<Typography variant="h6">잘 기억해뒀다가 소방관에게 알려주세요!</Typography>
+						</div>
+					</Alert>
+				</div>
+			)}
+			{buildingInfo && (
+				<div className="alert-container">
+					<Alert className="flex justify-center">
+						<div className="flex flex-row items-center m-2">
+							<InformationCircleIcon className="w-8 h-8 mr-2" color="yellow" />
+							<Typography variant="h3">여기는 삼성스토어 소행성지점 근처에요.</Typography>
+						</div>
+						<div className="flex justify-center">
+							<Typography variant="h6">잘 기억해뒀다가 소방관에게 알려주세요!</Typography>
+						</div>
+					</Alert>
+				</div>
+			)}
 		</SeqTwoWrapper>
 	);
 }
--- client/src/components/simsulationplay/Scene2/SeqTwo/style.ts
+++ client/src/components/simsulationplay/Scene2/SeqTwo/style.ts
@@ -1,13 +1,19 @@
 import styled from 'styled-components';
 
 export const SeqTwoWrapper = styled.div`
+	.alert-container {
+		position: absolute;
+		bottom: 65%;
+		right: 30%;
+	}
+
 	.left-point {
-		padding-top: 180px;
-		padding-left: 1050px;
+		padding-top: 300px;
+		padding-left: 1280px;
 	}
 
 	.right-point {
-		padding-top: 10px;
-		padding-left: 200px;
+		padding-top: 80px;
+		padding-left: 240px;
 	}
 `;
