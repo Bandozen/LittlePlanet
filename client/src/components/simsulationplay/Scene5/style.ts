66c9adfc7ff8713cf031b278e9b26309c4151593
wisdomx2 <wisdomx2@hanmail.net>
2023-11-15 14:23:57 +0900

    <feat>[#153-YJH] 아웃트로 수정

--- client/src/components/simsulationplay/Scene5/style.ts
+++ client/src/components/simsulationplay/Scene5/style.ts
@@ -1,9 +1,21 @@
 /* eslint-disable import/prefer-default-export */
 import styled from 'styled-components';
-import outrobgimg from '../../../assets/images/outrobackground.png';
+import outrobgimg1 from '../../../assets/images/outrobackground1.png';
+import outrobgimg2 from '../../../assets/images/outrobackground2.png';
 
 export const Scene5Wrapper = styled.div`
-	background-image: url(${outrobgimg});
+	background-image: url(${outrobgimg1});
+	background-size: 100%;
+	background-position: center;
+	background-repeat: no-repeat;
+	width: 100vw;
+	height: 100vh;
+	.btn-style {
+		margin-top: 50px;
+	}
+`;
+export const Bg2Wrapper = styled.div`
+	background-image: url(${outrobgimg2});
 	background-size: 100%;
 	background-position: center;
 	background-repeat: no-repeat;
